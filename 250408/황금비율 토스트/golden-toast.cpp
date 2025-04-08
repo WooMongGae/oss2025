ea3fa0496d4eb705419f187a78c4fd5c0380a64c
우성현 <104007513+WooMongGae@users.noreply.github.com>
2025-04-08 09:54:45 +0900

    Title: 황금비율 토스트, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250408/황금비율 토스트/golden-toast.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include <list>
+
+using namespace std;
+
+int n, m;
+string s;
+list<char> l;
+list<char> :: iterator it;
+
+int main() {
+    cin >> n >> m;
+    cin >> s;
+    for(char i : s){
+        l.push_back(i);
+    }
+    it = l.end();
+    
+
+    for (int i = 0; i < m; i++) {
+        char command;
+        cin >> command;
+        if (command == 'P') {
+            char c;
+            cin >> c;
+
+            l.insert(it, c);
+        }else if (command == 'L')
+        {
+          if(it!=l.begin()){
+            it--;
+          }
+        }else if (command == 'R')
+        {
+          if(it!=l.end()){
+            it++;
+          }
+        }else if (command == 'D')
+        {
+          if(it != l.end()){
+            it=l.erase(it);
+          }
+        }
+  
+        
+    }
+
+
+    for(it = l.begin();it!=l.end();it++){
+      cout<<*it;
+    }
+
+    // Please write your code here.
+
+    return 0;
+}
