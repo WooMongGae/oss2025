cfb553a078e429dc9477bbd721cac63724f6ddbb
우성현 <104007513+WooMongGae@users.noreply.github.com>
2025-04-15 15:01:36 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250415/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int arr[100];
+
+int f(int a){
+    if( a == 0) return arr[0];
+    return max(arr[a],f(a-1));
+}
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    cout<<f(n-1);
+    
+    // Please write your code here.
+
+    return 0;
+}
