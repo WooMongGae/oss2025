e189d567333ab434d1b89432a3bafbd155de6454
우성현 <104007513+WooMongGae@users.noreply.github.com>
2025-04-21 11:39:52 +0900

    Title: 기수 정렬 구현, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250421/기수 정렬 구현/implement-radix-sort.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n;
+int arr[100000];
+
+void number(){
+  int pos = 1;
+  for(int i =0;i<n;i++){
+    vector<int> v[10];
+    for(int i =0;i<n;i++){
+      v[arr[i]/pos%10].push_back(arr[i]);
+    }
+    int index = 0;
+    for(int i = 0;i<10;i++){
+      for(int j = 0;j<v[i].size();j++){
+        arr[index] = v[i][j]
+        index++;
+      }
+    }
+    pos *=10;
+  }
+
+
+
+}
+int main(){
+  cin>>n;
+  for(int i = 0;i<n;i++){
+    cin>>arr[i];
+  }
+
+  number();
+
+  for(int i = 0;i<n;i++){
+    cout<<arr[i]<<" ";
+  }
+}
