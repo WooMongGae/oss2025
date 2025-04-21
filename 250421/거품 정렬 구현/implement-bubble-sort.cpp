0dc159feecfe4c0466cfd781fbb3f373999bdc1c
우성현 <104007513+WooMongGae@users.noreply.github.com>
2025-04-21 20:32:37 +0900

    Title: 거품 정렬 구현, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250421/거품 정렬 구현/implement-bubble-sort.cpp
+++ 250421/거품 정렬 구현/implement-bubble-sort.cpp
@@ -5,11 +5,14 @@ int arr[100];
 
 void bubble(){
     bool sorted = true;
+    int temp;
     do{
         sorted = true;
         for(int i = 0;i<n-1;i++){
             if(arr[i]>arr[i+1]){
-                swap(arr[i],arr[i+1]);
+                temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
                 sorted = false;
             }
         }
@@ -27,4 +30,5 @@ int main(){
     for(int i = 0;i<n;i++){
         cout<<arr[i]<<" ";
     }
+
 }
