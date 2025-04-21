c272e0e7f1d1a18170eb89f60807d2bee766018f
우성현 <104007513+WooMongGae@users.noreply.github.com>
2025-04-21 16:46:59 +0900

    Title: 기수 정렬 구현, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 250421/기수 정렬 구현/implement-radix-sort.cpp
+++ 250421/기수 정렬 구현/implement-radix-sort.cpp
@@ -1,30 +1,31 @@
-#include <iostream>
+#include <iostrean>
+#include <algorithm>
 #include <vector>
-using namespace std;
-
 int n;
 int arr[100000];
 
 void number(){
-  int pos = 1;
-  for(int i =0;i<6;i++){
+  int p = 1;
+  for(int i = 0;i<6;i++){
     vector<int> v[10];
-    for(int i =0;i<n;i++){
-      v[arr[i]/pos%10].push_back(arr[i]);
+    for(int j = 0;j<n;j++){
+      v[arr[j]/p%10].push_back(arr[j]); 
     }
+
     int index = 0;
-    for(int i = 0;i<10;i++){
-      for(int j = 0;j<v[i].size();j++){
-        arr[index] = v[i][j];
-        index++;
+    for(int j = 0;j<10;j++){
+      for(int k = 0;k<v[j].size();k++){
+
+        arr[index++]=v[j][k];
       }
     }
-    pos *=10;
+
+    p *=10;
+
+
   }
-
-
-
 }
+
 int main(){
   cin>>n;
   for(int i = 0;i<n;i++){
@@ -36,4 +37,6 @@ int main(){
   for(int i = 0;i<n;i++){
     cout<<arr[i]<<" ";
   }
+
+
 }
