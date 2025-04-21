88e4aa7bca246840b250a049b11d29deecddaffc
우성현 <104007513+WooMongGae@users.noreply.github.com>
2025-04-21 15:39:03 +0900

    Title: 선택 정렬 구현, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250421/선택 정렬 구현/implement-selection-sort.cpp
+++ 250421/선택 정렬 구현/implement-selection-sort.cpp
@@ -7,7 +7,7 @@ int arr[100];
 void select(){
     for(int i = 0;i<n;i++){
         int key = i;
-        for(int j = 0;j<n;j++){
+        for(int j = i;j<n;j++){
             if(arr[j]<arr[key]){
                 key = j;
             }
