025adb3d31be81e108d65ac91dd5a75f4760de86
우성현 <104007513+WooMongGae@users.noreply.github.com>
2025-04-21 17:16:17 +0900

    Title: 삽입 정렬 구현, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250421/삽입 정렬 구현/implement-insertion-sort.cpp
+++ 250421/삽입 정렬 구현/implement-insertion-sort.cpp
@@ -5,11 +5,11 @@ int n;
 int arr[100];
 
 void insert(){
-    for(int i = 1; i <n;i++){
-        int key= arr[i];
-        int j = i-1;
+    for(int i =1;i<n;i++){
+        int key = arr[i];
+        int j =i-1;
         while(j>=0&&arr[j]>key){
-            arr[j+1] = arr[j];
+            arr[j+1]=arr[j];
             j--;
         }
         arr[j+1] = key;
@@ -20,10 +20,12 @@ int main(){
     cin>>n;
     for(int i = 0;i<n;i++){
         cin>>arr[i];
+    
     }
     insert();
     for(int i = 0;i<n;i++){
         cout<<arr[i]<<" ";
     }
+
 
 }
