6afeedf06c47a2fd163555ad2bf51746219ff758
우성현 <104007513+WooMongGae@users.noreply.github.com>
2025-04-21 17:51:28 +0900

    Title: 힙 정렬 구현, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250421/힙 정렬 구현/implement-heap-sort.cpp
+++ 250421/힙 정렬 구현/implement-heap-sort.cpp
@@ -1,34 +1,34 @@
-#include <iostream>
+#include<iostream>
 #include <algorithm>
 using namespace std;
+
 int n;
 int arr[100000];
 
-void heapify(int n,int i){
-    int j = i;
-    int a = i*2;
-    int b = i*2+1;
+void heapify(int max,int i){
+    int largest = i;
+    int a=i*2;
+    int b=i*2+1;
 
-    if(a<=n&&arr[j]<arr[a]){
-        j = a;
+    if(arr[largest]<arr[a]){
+        largest = a;
+    }
+    if(arr[largest]<arr[b]){
+        largest = b;
+    }
+    if(largest !=i){
+        swap(arr[largest],arr[i]);
+        heapify(max,largest);
     }
 
-    if(b<=n&&arr[j]<arr[b]){
-        j = b;
-    }
-
-    if(j!=i){
-        swap(arr[j],arr[i]);
-        heapify(n,j);
-    }
 }
 
 void heap(){
-    for(int i = n/2;i>=1;i--){
+    for(int i =n/2;i>=1;i--){
         heapify(n,i);
     }
 
-    for(int i = n;i>=1;i--){
+    for(int i = n;i>=2;i--){
         swap(arr[1],arr[i]);
         heapify(i-1,1);
     }
@@ -36,13 +36,12 @@ void heap(){
 
 int main(){
     cin>>n;
-    for(int i = 1;i<=n;i++){
+    for(int i = 0;i<n;i++){
         cin>>arr[i];
     }
-    heap();
 
-    for(int i = 1;i<=n;i++){
+    heap();
+    for(int i = 0;i<n;i++){
         cout<<arr[i]<<" ";
     }
-
 }
