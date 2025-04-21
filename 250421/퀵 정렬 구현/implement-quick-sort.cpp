9bcc032d46d09a1431762a05942f6b9bdc3a3a66
우성현 <104007513+WooMongGae@users.noreply.github.com>
2025-04-21 21:41:54 +0900

    Title: 퀵 정렬 구현, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 250421/퀵 정렬 구현/implement-quick-sort.cpp
+++ 250421/퀵 정렬 구현/implement-quick-sort.cpp
@@ -1,33 +1,29 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-
 int n;
 int arr[100000];
-
 int quick(int l,int h){
+    int pivot = arr[h];
     int i = l-1;
     int index = l;
-    int pivot = arr[h];
-
-    for(;index <h;index++){
+    for(;index<h;index++){
         if(arr[index]<pivot){
-            i++;
+            i++
             swap(arr[i],arr[index]);
         }
     }
+
     swap(arr[i+1],arr[h]);
     return i+1;
+
 }
 
 void quicksort(int l,int h){
     if(l<h){
-        int pos = quick(l,h);
-        quicksort(l,pos-1);
-        quicksort(pos+1,h);
-
-    }else{
-        return;
+        int index = quick(l,h);
+        quicksort(l,index-1);
+        quicksort(index+1,h);
     }
 }
 
@@ -38,9 +34,7 @@ int main(){
     }
 
     quicksort(0,n-1);
-
     for(int i = 0;i<n;i++){
         cout<<arr[i]<<" ";
     }
-
 }
