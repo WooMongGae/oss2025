c2c5ca6e4410663ba93edc1a0799315be112ae01
우성현 <104007513+WooMongGae@users.noreply.github.com>
2025-04-21 11:57:18 +0900

    Title: 병합 정렬 구현, Time: 14ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250421/병합 정렬 구현/implement-merge-sort.cpp
+++ 250421/병합 정렬 구현/implement-merge-sort.cpp
@@ -7,7 +7,7 @@ void merge(int l,int m,int h){
     int i = l;
     int j = m+1;
     int new_arr[100000];
-    int index = l;
+    int index =l;
 
     while(i<=m&&j<=h){
         if(arr[i]<arr[j]){
