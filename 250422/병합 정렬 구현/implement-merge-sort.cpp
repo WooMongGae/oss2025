6e664ea2a2de7700429a465d52ee230c9321c25c
우성현 <104007513+WooMongGae@users.noreply.github.com>
2025-04-22 11:58:49 +0900

    Title: 병합 정렬 구현, Time: 16ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 250422/병합 정렬 구현/implement-merge-sort.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int n;
+int arr[100];
+
+void merge(int l,int m,int h){
+    int i = l;
+    int j = m+1;
+    int index = l;
+    int new_arr[100000];
+    while(i<=m&&j<=h){
+        if(arr[i]<arr[j]){
+            new_arr[index++] = arr[i++];
+        }else{
+            new_arr[index++] = arr[j++];
+        }
+    }
+    while(i<=m){
+        new_arr[index++] = arr[i++];
+    }
+    while(j<=h){
+        new_arr[index++] = arr[j++];
+    }
+    for(int k = l;k<=h;k++){
+        arr[k] = new_arr[k];
+    }
+}
+
+void mergesort(int l,int h){
+    if(l<h){
+        int m = (l+h)/2;
+        mergesort(l,m);
+        mergesort(m+1,h);
+        merge(l,m,h);
+    }
+}
+
+int main(){
+    cin>>n;
+    for(int i = 0;i<n;i++){
+        cin>>arr[i];
+    }
+    mergesort(0,n-1);
+    for(int i = 0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
