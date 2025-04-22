ef896b9ffecc0130b09edd7d36ce56c2b8ce8102
우성현 <104007513+WooMongGae@users.noreply.github.com>
2025-04-22 15:28:56 +0900

    Title: 기수 정렬 구현, Time: 13ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250422/기수 정렬 구현/implement-radix-sort.cpp
+++ 250422/기수 정렬 구현/implement-radix-sort.cpp
@@ -4,20 +4,21 @@ using namespace std;
 int n;
 int arr[100000];
 void number(){
-  int p =1;
+  int p = 1;
   for(int i = 0;i<6;i++){
     vector<int> v[10];
     for(int j = 0;j<n;j++){
-      v[arr[j]/p%10].push_back(arr[j]);
+      v[arr[j]/p%10].push_back(arr[i]);
     }
     int index = 0;
-    for(int x= 0;x<10;x++){
+    for(int x = 0;x<10;x++){
       for(int y = 0;y<v[x].size();y++){
         arr[index++] = v[x][y];
       }
     }
-    p *=10;
+    p*=10;
   }
+
 }
 
 int main(){
@@ -29,5 +30,4 @@ int main(){
   for(int i = 0;i<n;i++){
     cout<<arr[i]<<" ";
   }
-
 }
